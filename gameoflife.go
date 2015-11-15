e4a8a4310480822500b5897b2fffb2fa0191ae64
Leandro Santiago <leandrosansilva@gmail.com>
2015-11-15 18:50:46 +0100

    Everything is f*cked up :-(

--- gameoflife.go
+++ gameoflife.go
@@ -2,76 +2,276 @@ package gameoflife
 
 import (
 	"errors"
+	"log"
 )
 
+type CellState int
+
+const (
+	// Not used by the cell, but informs there
+	// is no neighbour
+	INVALID_NEIGHBOUR CellState = iota
+
+	ACTIVE_CELL   CellState = iota
+	INACTIVE_CELL CellState = iota
+)
+
+type NeighboursStates [8]CellState
+
 type Cell struct {
-	Active bool
+	State CellState
 }
 
-type Coordinate struct {
+type Coord struct {
 	X, Y int
 }
 
+type WorldMatrix [][]Cell
+
 type World struct {
-	Matrix [][]Cell
+	// The current Matrix and the next generation one
+	Matrices         [2]WorldMatrix
+	UsingFirstMatrix bool
 }
 
-func NewActiveCell() Cell {
-	return Cell{true}
+type Generator struct {
+	World *World
+	Rules []Rule
 }
 
-func NewInactiveCell() Cell {
-	return Cell{false}
+type RuleFilter func(coord Coord) bool
+
+type RuleApplier func(neighbours NeighboursStates, live bool) bool
+
+type Rule struct {
+	Filter  RuleFilter
+	Applier RuleApplier
 }
 
-func (this *Cell) IsActive() bool {
-	return this.Active
+func NewLiveCell() Cell {
+	return Cell{ACTIVE_CELL}
 }
 
-func NewCoord(x, y int) Coordinate {
-	return Coordinate{x, y}
+func NewDeadCell() Cell {
+	return Cell{INACTIVE_CELL}
 }
 
-func (this *Coordinate) Get() (x, y int) {
+func (this *Cell) IsLive() bool {
+	return this.State == ACTIVE_CELL
+}
+
+func NewCoord(x, y int) Coord {
+	return Coord{x, y}
+}
+
+func (this Coord) North() Coord {
+	x, y := this.Get()
+	return NewCoord(x, y-1)
+}
+
+func (this Coord) South() Coord {
+	x, y := this.Get()
+	return NewCoord(x, y+1)
+}
+
+func (this Coord) West() Coord {
+	x, y := this.Get()
+	return NewCoord(x-1, y)
+}
+
+func (this Coord) East() Coord {
+	x, y := this.Get()
+	return NewCoord(x+1, y)
+}
+
+func (this Coord) NorthWest() Coord {
+	return this.North().West()
+}
+
+func (this Coord) NorthEast() Coord {
+	return this.North().East()
+}
+
+func (this Coord) SouthWest() Coord {
+	return this.South().West()
+}
+
+func (this Coord) SouthEast() Coord {
+	return this.South().East()
+}
+
+func (this *Coord) Get() (x, y int) {
 	return this.X, this.Y
+}
+
+func CreateMatrix(h, w int) WorldMatrix {
+	matrix := make(WorldMatrix, w)
+
+	for iw, _ := range matrix {
+		matrix[iw] = make([]Cell, h)
+		for ih, _ := range matrix[iw] {
+			matrix[iw][ih] = NewDeadCell()
+		}
+	}
+
+	return matrix
+}
+
+func (this *WorldMatrix) RefToCell(coord Coord) *Cell {
+	x, y := coord.Get()
+	return &(*this)[y][x]
 }
 
 func NewWorld(h, w int) (World, error) {
 	if h > 0 && w > 0 {
-		matrix := make([][]Cell, w)
-
-		for index, _ := range matrix {
-			matrix[index] = make([]Cell, h)
-		}
-
-		return World{matrix}, nil
+		return World{[2]WorldMatrix{CreateMatrix(h, w), CreateMatrix(h, w)}, true}, nil
 	}
 
 	return World{}, errors.New("Impossible world")
 }
 
-func (this *World) IsCoordValid(coord Coordinate) bool {
-	x, y := coord.Get()
+func (this *World) GetMatrices() (live, inactive *WorldMatrix) {
+	if this.UsingFirstMatrix {
+		return &this.Matrices[0], &this.Matrices[1]
+	}
 
-	return x >= 0 && x < len(this.Matrix) && y >= 0 && y < len(this.Matrix[0])
-
+	return &this.Matrices[1], &this.Matrices[0]
 }
 
-func (this *World) IsCellActive(coord Coordinate) (bool, error) {
+func (this *World) GetLiveMatrix() *WorldMatrix {
+	matrix, _ := this.GetMatrices()
+	return matrix
+}
+
+func (this *World) GetDeadMatrix() *WorldMatrix {
+	_, matrix := this.GetMatrices()
+	return matrix
+}
+
+func (this *World) SwitchMatrices() {
+	this.UsingFirstMatrix = !this.UsingFirstMatrix
+}
+
+func (this *World) IsCoordValid(coord Coord) bool {
+	x, y := coord.Get()
+	return x >= 0 && x < len(this.Matrices[0]) && y >= 0 && y < len(this.Matrices[0][0])
+}
+
+func (this *World) IsCellLive(coord Coord) (bool, error) {
 	if this.IsCoordValid(coord) {
 		x, y := coord.Get()
-		return this.Matrix[x][y].IsActive(), nil
+		return (*this.GetLiveMatrix())[x][y].IsLive(), nil
 	}
 
 	return false, errors.New("Invalid coord")
 }
 
-func (this *World) ActivateCell(coord Coordinate) error {
+func (this *World) ActivateCell(coord Coord) error {
 	if this.IsCoordValid(coord) {
 		x, y := coord.Get()
-		this.Matrix[x][y] = NewActiveCell()
+		(*this.GetDeadMatrix())[x][y] = NewLiveCell()
 		return nil
 	}
 
 	return errors.New("Invalid coord")
+}
+
+func (this *World) ForEachCoordinate(f func(coord Coord)) {
+	for iY, _ := range this.Matrices[0] {
+		for iX, _ := range this.Matrices[0][iY] {
+			f(NewCoord(iX, iY))
+		}
+	}
+}
+
+func (this *World) GetCellState(coord Coord) CellState {
+	if this.IsCoordValid(coord) {
+		x, y := coord.Get()
+
+		if (*this.GetLiveMatrix())[y][x].IsLive() {
+			return ACTIVE_CELL
+		}
+
+		return INACTIVE_CELL
+	}
+
+	return INVALID_NEIGHBOUR
+}
+
+func (this *World) GetCellNeighbours(coord Coord) NeighboursStates {
+	return NeighboursStates{
+		this.GetCellState(coord.NorthWest()),
+		this.GetCellState(coord.North()),
+		this.GetCellState(coord.NorthEast()),
+		this.GetCellState(coord.East()),
+		this.GetCellState(coord.SouthEast()),
+		this.GetCellState(coord.South()),
+		this.GetCellState(coord.SouthWest()),
+		this.GetCellState(coord.West()),
+	}
+}
+
+func NewGenerator(world *World) Generator {
+	rules := make([]Rule, 0)
+
+	countNeighbours := func(neighbours NeighboursStates, expectedState CellState) int {
+		// NOTE: this is similar to reduce(sum)
+		count := 0
+
+		for _, state := range neighbours {
+			if state == expectedState {
+				count++
+			}
+		}
+
+		return count
+	}
+
+	rules = append(rules, NewRule(func(coord Coord) bool {
+		// Applies to live cells
+		live, _ := world.IsCellLive(coord)
+		return live
+	}, func(neighbours NeighboursStates, live bool) bool {
+		nLiveNeighbours := countNeighbours(neighbours, ACTIVE_CELL)
+		return nLiveNeighbours == 2 || nLiveNeighbours == 3
+	}))
+
+	rules = append(rules, NewRule(func(coord Coord) bool {
+		// Applies to dead cells
+		live, _ := world.IsCellLive(coord)
+		return !live
+	}, func(neighbours NeighboursStates, live bool) bool {
+		return countNeighbours(neighbours, ACTIVE_CELL) == 3
+	}))
+
+	return Generator{world, rules}
+}
+
+func (this *Generator) Step() {
+	_, inactiveMatrix := this.World.GetMatrices()
+
+	this.World.ForEachCoordinate(func(coord Coord) {
+		isLive, _ := this.World.IsCellLive(coord)
+
+		for _, rule := range this.Rules {
+			if rule.ApplyToCell(coord, isLive, this.World.GetCellNeighbours(coord)) {
+				log.Printf("Spawning %s\n", coord)
+				*(inactiveMatrix.RefToCell(coord)) = NewLiveCell()
+				return
+			}
+		}
+
+		log.Printf("Killing %s\n", coord)
+		*(inactiveMatrix.RefToCell(coord)) = NewDeadCell()
+	})
+
+	this.World.SwitchMatrices()
+}
+
+func NewRule(filter RuleFilter, applier RuleApplier) Rule {
+	return Rule{filter, applier}
+}
+
+func (this *Rule) ApplyToCell(coord Coord, live bool, neighbours NeighboursStates) bool {
+	return this.Filter(coord) && this.Applier(neighbours, live)
 }
--- gameoflife_test.go
+++ gameoflife_test.go
@@ -9,23 +9,79 @@ import (
 func TestGameOfLife(t *testing.T) {
 
 	Convey("Test Cells", t, func() {
-		Convey("Test single active cell", func() {
-			cell := NewActiveCell()
-			So(cell.IsActive(), ShouldBeTrue)
+		Convey("Test single live cell", func() {
+			cell := NewLiveCell()
+			So(cell.IsLive(), ShouldBeTrue)
 		})
 
-		Convey("Test single inactive cell", func() {
-			cell := NewInactiveCell()
-			So(cell.IsActive(), ShouldBeFalse)
+		Convey("Test single dead cell", func() {
+			cell := NewDeadCell()
+			So(cell.IsLive(), ShouldBeFalse)
 		})
 	})
 
-	Convey("Test Coordinate", t, func() {
-		Convey("Basic Coordinate", func() {
+	Convey("Test Coord", t, func() {
+		Convey("Basic Coord", func() {
 			coord := NewCoord(1, 3)
 			x, y := coord.Get()
 			So(x, ShouldEqual, 1)
 			So(y, ShouldEqual, 3)
+		})
+
+		Convey("North", func() {
+			coord := NewCoord(0, 0).North()
+			x, y := coord.Get()
+			So(x, ShouldEqual, 0)
+			So(y, ShouldEqual, -1)
+		})
+
+		Convey("South", func() {
+			coord := NewCoord(0, 0).South()
+			x, y := coord.Get()
+			So(x, ShouldEqual, 0)
+			So(y, ShouldEqual, 1)
+		})
+
+		Convey("West", func() {
+			coord := NewCoord(0, 0).West()
+			x, y := coord.Get()
+			So(x, ShouldEqual, -1)
+			So(y, ShouldEqual, 0)
+		})
+
+		Convey("East", func() {
+			coord := NewCoord(0, 0).East()
+			x, y := coord.Get()
+			So(x, ShouldEqual, 1)
+			So(y, ShouldEqual, 0)
+		})
+
+		Convey("North-West", func() {
+			coord := NewCoord(0, 0).NorthWest()
+			x, y := coord.Get()
+			So(x, ShouldEqual, -1)
+			So(y, ShouldEqual, -1)
+		})
+
+		Convey("North-East", func() {
+			coord := NewCoord(0, 0).NorthEast()
+			x, y := coord.Get()
+			So(x, ShouldEqual, 1)
+			So(y, ShouldEqual, -1)
+		})
+
+		Convey("South-West", func() {
+			coord := NewCoord(0, 0).SouthWest()
+			x, y := coord.Get()
+			So(x, ShouldEqual, -1)
+			So(y, ShouldEqual, 1)
+		})
+
+		Convey("South-East", func() {
+			coord := NewCoord(0, 0).SouthEast()
+			x, y := coord.Get()
+			So(x, ShouldEqual, 1)
+			So(y, ShouldEqual, 1)
 		})
 	})
 
@@ -40,17 +96,17 @@ func TestGameOfLife(t *testing.T) {
 			So(err, ShouldEqual, nil)
 
 			Convey("Invalid X", func() {
-				_, err := world.IsCellActive(NewCoord(1, 0))
+				_, err := world.IsCellLive(NewCoord(1, 0))
 				So(err, ShouldResemble, errors.New("Invalid coord"))
 			})
 
 			Convey("Invalid Y", func() {
-				_, err := world.IsCellActive(NewCoord(0, 1))
+				_, err := world.IsCellLive(NewCoord(0, 1))
 				So(err, ShouldResemble, errors.New("Invalid coord"))
 			})
 
 			Convey("Invalid X and Y", func() {
-				_, err := world.IsCellActive(NewCoord(1, -3))
+				_, err := world.IsCellLive(NewCoord(1, -3))
 				So(err, ShouldResemble, errors.New("Invalid coord"))
 			})
 		})
@@ -58,21 +114,187 @@ func TestGameOfLife(t *testing.T) {
 		Convey("Activation of cell in trivial world", func() {
 			world, _ := NewWorld(1, 1)
 
-			Convey("Cell on 0,0 is inactive", func() {
-				active, _ := world.IsCellActive(NewCoord(0, 0))
-				So(active, ShouldBeFalse)
+			Convey("Cell on 0,0 is dead", func() {
+				live, _ := world.IsCellLive(NewCoord(0, 0))
+				So(live, ShouldBeFalse)
 			})
 
 			Convey("Fail to activate cell", func() {
 				err := world.ActivateCell(NewCoord(1, 1))
 				So(err, ShouldResemble, errors.New("Invalid coord"))
+				world.SwitchMatrices()
 			})
 
-			Convey("Active cell in 0,0", func() {
+			Convey("Live cell in 0,0", func() {
 				err := world.ActivateCell(NewCoord(0, 0))
 				So(err, ShouldEqual, nil)
-				active, _ := world.IsCellActive(NewCoord(0, 0))
-				So(active, ShouldBeTrue)
+				world.SwitchMatrices()
+				live, _ := world.IsCellLive(NewCoord(0, 0))
+				So(live, ShouldBeTrue)
+			})
+		})
+	})
+
+	Convey("Test Rules", t, func() {
+		Convey("Always live regardless of neighbours", func() {
+			rule := NewRule(func(coord Coord) bool {
+				return true
+			}, func(neighbours NeighboursStates, live bool) bool {
+				return true
+			})
+
+			So(rule.ApplyToCell(NewCoord(0, 0), true, NeighboursStates{}), ShouldBeTrue)
+		})
+
+		Convey("Always die regardless of neighbours", func() {
+			rule := NewRule(func(coord Coord) bool {
+				return true
+			}, func(neighbours NeighboursStates, live bool) bool {
+				return false
+			})
+
+			So(rule.ApplyToCell(NewCoord(0, 0), true, NeighboursStates{}), ShouldBeFalse)
+		})
+
+		Convey("Die because rule does not apply", func() {
+			rule := NewRule(func(coord Coord) bool {
+				x, y := coord.Get()
+				return x == 0 && y == 0
+			}, func(neighbours NeighboursStates, live bool) bool {
+				return true
+			})
+
+			So(rule.ApplyToCell(NewCoord(1, 1), true, NeighboursStates{}), ShouldBeFalse)
+		})
+	})
+
+	Convey("Test Neighbours", t, func() {
+		Convey("All neighbours are invalid in the trivial world", func() {
+			world, _ := NewWorld(1, 1)
+			neighbours := world.GetCellNeighbours(NewCoord(0, 0))
+
+			So(neighbours, ShouldEqual, NeighboursStates{
+				INVALID_NEIGHBOUR,
+				INVALID_NEIGHBOUR,
+				INVALID_NEIGHBOUR,
+				INVALID_NEIGHBOUR,
+				INVALID_NEIGHBOUR,
+				INVALID_NEIGHBOUR,
+				INVALID_NEIGHBOUR,
+				INVALID_NEIGHBOUR,
+			})
+		})
+
+		Convey("Smallest square valid world", func() {
+			// TODO: test more...
+			world, _ := NewWorld(3, 3)
+
+			So(world.GetCellNeighbours(NewCoord(1, 1)), ShouldEqual, NeighboursStates{
+				INACTIVE_CELL,
+				INACTIVE_CELL,
+				INACTIVE_CELL,
+				INACTIVE_CELL,
+				INACTIVE_CELL,
+				INACTIVE_CELL,
+				INACTIVE_CELL,
+				INACTIVE_CELL,
+			})
+
+			So(world.GetCellNeighbours(NewCoord(0, 0)), ShouldEqual, NeighboursStates{
+				INVALID_NEIGHBOUR,
+				INVALID_NEIGHBOUR,
+				INVALID_NEIGHBOUR,
+				INACTIVE_CELL,
+				INACTIVE_CELL,
+				INACTIVE_CELL,
+				INVALID_NEIGHBOUR,
+				INVALID_NEIGHBOUR,
+			})
+		})
+	})
+
+	Convey("Test Generator", t, func() {
+		Convey("Cell alone dies", func() {
+			world, _ := NewWorld(3, 3)
+			world.ActivateCell(NewCoord(1, 1))
+
+			generator := NewGenerator(&world)
+			generator.Step()
+
+			world.ForEachCoordinate(func(coord Coord) {
+				live, err := world.IsCellLive(coord)
+				So(err, ShouldEqual, nil)
+				So(live, ShouldBeFalse)
+			})
+		})
+
+		Convey("Cell with a single neighbour dies", func() {
+			world, _ := NewWorld(3, 3)
+			world.ActivateCell(NewCoord(1, 0))
+			world.ActivateCell(NewCoord(1, 1))
+
+			generator := NewGenerator(&world)
+			generator.Step()
+
+			world.ForEachCoordinate(func(coord Coord) {
+				live, err := world.IsCellLive(coord)
+				So(err, ShouldEqual, nil)
+				So(live, ShouldBeFalse)
+			})
+		})
+
+		Convey("Three cells inline rotate", func() {
+			world, _ := NewWorld(3, 3)
+			world.ActivateCell(NewCoord(1, 0))
+			world.ActivateCell(NewCoord(1, 1))
+			world.ActivateCell(NewCoord(1, 2))
+
+			generator := NewGenerator(&world)
+			generator.Step()
+
+			Convey("0x0", func() {
+				live, _ := world.IsCellLive(NewCoord(0, 0))
+				So(live, ShouldBeFalse)
+			})
+
+			Convey("1x0", func() {
+				live, _ := world.IsCellLive(NewCoord(1, 0))
+				So(live, ShouldBeFalse)
+			})
+
+			Convey("2x0", func() {
+				live, _ := world.IsCellLive(NewCoord(2, 0))
+				So(live, ShouldBeFalse)
+			})
+
+			Convey("0x1", func() {
+				live, _ := world.IsCellLive(NewCoord(0, 1))
+				So(live, ShouldBeTrue)
+			})
+
+			Convey("1x1", func() {
+				live, _ := world.IsCellLive(NewCoord(1, 1))
+				So(live, ShouldBeTrue)
+			})
+
+			Convey("2x1", func() {
+				live, _ := world.IsCellLive(NewCoord(2, 1))
+				So(live, ShouldBeTrue)
+			})
+
+			Convey("0x2", func() {
+				live, _ := world.IsCellLive(NewCoord(0, 2))
+				So(live, ShouldBeFalse)
+			})
+
+			Convey("1x2", func() {
+				live, _ := world.IsCellLive(NewCoord(1, 2))
+				So(live, ShouldBeFalse)
+			})
+
+			Convey("2x2", func() {
+				live, _ := world.IsCellLive(NewCoord(2, 2))
+				So(live, ShouldBeFalse)
 			})
 		})
 	})
